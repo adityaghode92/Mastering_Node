59d91ec0b7f6cbf6d99d99497db98eaf3c59510a
Adityaghode02 <adityaghode92@gmail.com>
2024-07-08 18:36:45 +0530

    Operating System

---
+++ NodeJs/3_OS.js
@@ -0,0 +1,16 @@
+
+// const os = require("os");
+
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.type());
+// console.log(os.platform());
+// console.log(os.tmpdir());
+
+
+// const freeRam = os.freemem();
+// console.log(freeRam);
+
+// const totalRam = os.totalmem();
+// console.log(totalRam);
+
