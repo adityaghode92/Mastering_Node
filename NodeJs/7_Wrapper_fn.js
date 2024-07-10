4c2e00170b28de8744fa7304d7a5f966adb3b71b
Adityaghode02 <adityaghode92@gmail.com>
2024-07-10 11:39:55 +0530

    Wrapper module

---
+++ NodeJs/7_Wrapper_fn.js
@@ -0,0 +1,12 @@
+
+(function(module,require,exports,dir__name,file__name){
+    //data is private
+    const s = "nitrosence"
+    console.log(s);
+})();
+
+
+//it is IEFE (immediatly invoked function expression)
+(function(){
+   // IFIE
+})();
