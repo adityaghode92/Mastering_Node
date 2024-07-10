2654014e1cc052c1f52b9ce36dfecfb7c6255827
Adityaghode02 <adityaghode92@gmail.com>
2024-07-10 12:57:52 +0530

    server created

---
+++ NodeJs/8_web_server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    res.end("Hellow there im from other side");
+});
+
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("I am listing");
+});
